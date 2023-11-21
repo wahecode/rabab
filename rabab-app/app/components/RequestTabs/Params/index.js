6b631f412fe83a3d59fb712acf40c6320acfc07e
Arvind Chandok <arvind.chandok@gmail.com>
2023-11-21 10:00:39 -0600

    Added params title row

--- rabab-app/app/components/RequestTabs/Params/index.js
+++ rabab-app/app/components/RequestTabs/Params/index.js
@@ -4,6 +4,15 @@ export default function Params({ request }) {
 
     return (
         <div>
+            <div className='flex'>
+                <div className=' pl-2 pr-2 border border-gray-700 pt-1 pb-1'>
+                    <input type='checkbox' />
+                </div>
+                <div className='w-full border border-t-0 border-l-0 border-gray-700 p-1' >Key</div>
+                <div className='w-full border border-t-0 border-l-0 border-gray-700 p-1' >Value</div>
+                <div className='w-full border border-t-0 border-l-0 border-gray-700 p-1' >Description</div>
+            </div>
+
             <ParamsRow />
             <ParamsRow />
             <ParamsRow />
@@ -28,7 +37,7 @@ function ParamsRow() {
 
 function InputColumn({ name }) {
     return (
-        <div className='w-full border border-gray-700 p-1' >
+        <div className='w-full border border-t-0 border-l-0 border-gray-700 p-1' >
             <input type='text' name={name} id={name} className='dark:bg-black w-full h-full outline-none dark:focus:bg-gray-700' />
         </div>
     )
