0d77e4723d84400e9819bbdb2dc7ab417fe8b9f5
Arvind Chandok <arvind.chandok@gmail.com>
2023-12-04 17:43:21 -0600

    Updated request slice to add unique name

--- rabab-app/app/slices/requestSlice.js
+++ rabab-app/app/slices/requestSlice.js
@@ -7,9 +7,17 @@ export const slice = createSlice({
     },
     reducers: {
         addNewRequest: (state, action) => {
+            let name = 'New Request'
+            if (state.requests) {
+                const count = state.requests.filter(req => req.name.startsWith('New Request')).length;
+                if (count > 0) {
+                    name = 'New Request ' + (count);
+                }
+            }
+
             state.requests.push({
                 id: Math.random(),
-                name: 'New Request',
+                name: name,
                 collectionId: action.payload.collectionId,
                 uri: null,
                 method: 'GET',
