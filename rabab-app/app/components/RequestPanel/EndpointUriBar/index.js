e3b3f4555d5ae519d4bba4840721429261ce0723
Arvind Chandok <arvind.chandok@gmail.com>
2023-12-04 18:15:48 -0600

    corrected endpint uri component

--- rabab-app/app/components/RequestPanel/EndpointUriBar/index.js
+++ rabab-app/app/components/RequestPanel/EndpointUriBar/index.js
@@ -1,18 +1,26 @@
 import Button from '@/app/common/Button'
 import InputText from '@/app/common/InputText'
+import { useState } from 'react';
 
 export function EndPointUriBar({ request, onChange, onSend, onSave }) {
+    const [method, setMethod] = useState(request.method);
+    const [uri, setUri] = useState(request.uri);
+
     const onChangeHandler = (name, newValue) => {
         if (name == 'method') {
             request.method = newValue
+            setMethod(newValue);
         } else {
             request.uri = newValue
+            setUri(newValue);
         }
 
         if (onChange) {
             onChange(request)
         }
     }
+
+
 
     return (
         <div className="flex w-full p-1 border border-rabab">
@@ -27,7 +35,8 @@ export function EndPointUriBar({ request, onChange, onSend, onSave }) {
                 </select>
             </div>
             <div className="w-full">
-                <InputText name="uri" value={request.uri} onChange={onChangeHandler} />
+                <InputText name="uri" value={uri}
+                    onChange={(value) => onChangeHandler('uri', value)} />
             </div>
             <div className="pr-1">
                 <Button onClick={onSend}>Send</Button>
